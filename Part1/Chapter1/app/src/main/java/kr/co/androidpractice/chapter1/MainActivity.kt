18e9612e7fd02f4f9c12a5a50cb0adc6bacc6ff3
jonghyeok.baek@geoplan.kr <jonghyeok.baek@geoplan.kr>
2024-06-13 11:01:04 +0900

    first commit

---
+++ Part1/Chapter1/app/src/main/java/kr/co/androidpractice/chapter1/MainActivity.kt
@@ -0,0 +1,78 @@
+package kr.co.androidpractice.chapter1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.co.androidpractice.chapter1.ui.theme.Chapter1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Chapter1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NumberCounter()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NumberCounter() {
+    var number by remember { mutableIntStateOf(0) }
+
+    Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Number: $number")
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Button(onClick = { number++ }) {
+                    Text(text = "숫자 증가")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { number-- }) {
+                    Text(text = "숫자 감소")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Chapter1Theme {
+        NumberCounter()
+    }
+}
